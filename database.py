5219d64516ebe2f319c4fb19ce1d0d1692d3d588
nhailongRes <nhailong6868@gmail.com>
2026-09-15 12:50:45 +1000

    Add testing with TESTCLIENT

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 import os
 from dotenv import load_dotenv
@@ -7,11 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 engine = create_engine(database_url, echo=True)
+SessionLocal = sessionmaker(bind = engine)
 
 class Base(DeclarativeBase):
     pass
 def get_db():
-    session = Session(engine)
+    session = SessionLocal()
     try:
         yield session
     finally:
---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import os 
+import pytest
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+
+load_dotenv()
+
+
+from database import Base, get_db
+from main import app
+import model
+
+
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+test_engine = create_engine(TEST_DATABASE_URL, echo=True)
+TestSessionLocal = sessionmaker(bind=test_engine)
+
+
+@pytest.fixture
+def db_session():
+    Base.metadata.create_all(bind=test_engine)
+    db = TestSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=test_engine)
+
+
+@pytest.fixture 
+def client(db_session):
+    def override_get_db():
+        yield db_session
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    app.dependency_overrides.clear()
